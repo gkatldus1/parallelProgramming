43d3006405a9ab2aa585e29f7c546f01627d5a49
gkatldus1 <gkatldus1@gmail.com>
2021-05-07 19:53:47 +0900

    error fixing

--- finalCheck.c
+++ finalCheck.c
@@ -13,10 +13,10 @@ int main(int argc, char** argv){
 	MPI_Status status;
 	clock_t start;
 	clock_t end;
-	long *time;
-	int loop = 10000;
-	long sum = 0;
-	time = malloc(loop*sizeof(long));
+//	long *time;
+	int loop = 1;
+	long time = 0;
+//	time = malloc(loop*sizeof(long));
 	send_msg = malloc(num*sizeof(int));
 	recv_msg = malloc(num*sizeof(int));
 	MPI_Init(NULL,NULL);
@@ -40,20 +40,20 @@ int main(int argc, char** argv){
 			start = clock();	
 	
 			MPI_Send(send_msg, num, MPI_INT, n, 0, MPI_COMM_WORLD);	
-			
+			free(send_msg);
 			MPI_Recv(recv_msg, num, MPI_INT, n, 0, MPI_COMM_WORLD, &status);
 			end = clock();
+			free(recv_msg);
+			time = end - start;
 			
-			time[i] = end - start;
-		
 		}
 	
 		if(rank == 1)
 		{	
 			MPI_Recv(recv_msg, num, MPI_INT, n, 0, MPI_COMM_WORLD, &status);
-			
+			free(recv_msg);
 			MPI_Send(send_msg, num, MPI_INT, n, 0, MPI_COMM_WORLD);
-			
+			free(send_msg);
 		}
 //		for(int i = 0; i < num; i++){
 //			printf("rank:%d, recvmessage: %d\n", rank, recv_msg[i]);
@@ -62,16 +62,13 @@ int main(int argc, char** argv){
 	}
 	if(rank == 0)
 	{	
-		for(i=0; i< loop; i++)
-		{	
-			sum += time[i];
-		}
-		printf("average: %ld\n", sum/loop);
+		
+		printf("result: %ld\n", time);
 	}
 
-	free(send_msg);
-	free(recv_msg);
-	free(time);
+//	free(send_msg);
+//	free(recv_msg);
+//	free(time);
 
 	MPI_Finalize();
 
