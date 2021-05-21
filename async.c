5d070a7a8beafb284714c2e355154e364ceb8c95
gkatldus1 <gkatldus1@gmail.com>
2021-05-21 23:09:24 +0900

    async communication between process

--- async.c
+++ async.c
@@ -5,6 +5,9 @@ int main(int argc, char** argv)
 {
 
 	int n_ranks = 0, rank = 0;
+	
+	MPI_Request request;
+
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &n_ranks);
@@ -23,15 +26,18 @@ int main(int argc, char** argv)
 	if (rank == 0)
 	{
 		char *message = "Hello, world!\n";
-		MPI_Send(message, 16, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+		MPI_Status status;
+		MPI_Isend(message, 16, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &request);
+		MPI_Wait(&request, &status);
 	}
 
 	if (rank == 1)
 	{	
 		char message[16];
 		MPI_Status status;
-		MPI_Recv(message, 16, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
-		
+		MPI_Irecv(message, 16, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &request);
+
+		MPI_Wait(&request, &status);		
 		printf("%s\n", message);
 
 
